a055d412e4e9f55d2cbf88b6628ec36f2a8c2717
Questionologist <58661494+Questionologist@users.noreply.github.com>
2022-12-15 20:14:11 +0800

    Update datastore.js

--- datastore.js
+++ datastore.js
@@ -1,7 +1,7 @@
 const Store = require('electron-store');
 
 const store = new Store();
-function test(){
+function test(){ 
   store.set('unicorn', '🦄');
   console.log(store.get('unicorn'));
   //=> '🦄'
